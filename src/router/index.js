3d44d08d6017920b3eac004a90b4da38528c3a2d
Mahesaadikusuma <mahesaadikuzuma@gmail.com>
2025-04-30 22:43:48 +0700

    feat: halaman login dan register

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
 import HomeView from '@/views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
+import RegisterView from '@/views/RegisterView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
 
@@ -9,6 +11,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
     },
     {
       // path: '/about',
